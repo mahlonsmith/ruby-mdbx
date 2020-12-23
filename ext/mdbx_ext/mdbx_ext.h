ceb92fad16af3c9b920f1b3b44533c92b3cd75e3
mahlon@martini.nu <mahlon@martini.nu>
2020-12-23 01:10:19 +0000

    Expose some mdbx statistics/metadata to ruby.
      - Add anonymous structs for settings and state to the internal db
        instance struct.
      - Allow tweaking the max_readers setting on db open.
      - Allow altering database sizing/geometry values.
      - Start sketching out some transaction re-entrancy.
      - Refactor initialization slightly, just use the db struct
        instead of additional unnecessary vars.
    
    FossilOrigin-Name: 513f02946f7dc39001c402c1adf0697bb2852ba867685b35adbccaaaf43c6e15

--- ext/mdbx_ext/mdbx_ext.h
+++ ext/mdbx_ext/mdbx_ext.h
@@ -7,6 +7,39 @@
 #ifndef MDBX_EXT_0_9_2
 #define MDBX_EXT_0_9_2
 
+#define RMDBX_TXN_ROLLBACK 0
+#define RMDBX_TXN_COMMIT 1
+
+/*
+ * A struct encapsulating an instance's DB
+ * state and settings.
+ */
+struct rmdbx_db {
+	MDBX_env *env;
+	MDBX_dbi dbi;
+	MDBX_txn *txn;
+	MDBX_cursor *cursor;
+
+    struct {
+       int env_flags;
+       int mode;
+       int open;
+       int max_collections;
+       int max_readers;
+       uint64_t max_size;
+    } settings;
+
+    struct {
+       int open;
+    } state;
+
+	char *path;
+	char *subdb;
+};
+typedef struct rmdbx_db rmdbx_db_t;
+
+static const rb_data_type_t rmdbx_db_data;
+extern void rmdbx_free( void *db ); /* forward declaration for the allocator */
 
 /* ------------------------------------------------------------
  * Globals
@@ -23,6 +56,10 @@ extern VALUE rmdbx_eRollback;
  * ------------------------------------------------------------ */
 extern void Init_rmdbx ( void );
 extern void rmdbx_init_database ( void );
+extern void rmdbx_open_txn( rmdbx_db_t*, int );
+extern void rmdbx_close_txn( rmdbx_db_t*, int );
+
+extern VALUE rmdbx_gather_stats( rmdbx_db_t* );
 
 
 #endif /* define MDBX_EXT_0_9_2 */
